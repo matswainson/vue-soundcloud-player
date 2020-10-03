61f6854ccc0b6dc96507b2136c00055125aaaa3e
matswainson <m@ms-MacBook-Pro.local>
2020-10-03 10:00:24 +0200

    Emit ready event when player is loaded

---
+++ src/Events.vue
@@ -0,0 +1,22 @@
+<template>
+  <SoundCloud
+    playlist="73448639"
+    @ready="ready"
+  />
+</template>
+
+<script>
+import SoundCloud from './SoundCloud'
+
+export default {
+  name: 'Events',
+  components: {
+    SoundCloud
+  },
+  methods: {
+    ready() {
+      console.log('player ready')
+    }
+  }
+}
+</script>
--- src/index.stories.js
+++ src/index.stories.js
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import Events from './Events.vue';
 import SoundCloud from './SoundCloud.vue';
 
 const fontStack = 'BlinkMacSystemFont,-apple-system,Segoe UI,Roboto,Helvetica,Arial,sans-serif';
@@ -6,29 +7,42 @@ const StyleWrapper = Vue.component('StyleWrapper', {
   template: `<div style="font-family: ${fontStack}"><slot /></div>`
 });
 
+const components = {
+  SoundCloud,
+  StyleWrapper
+};
+
 export default { title: 'SoundCloud' };
 
 export const standard = () => ({
-  components: { SoundCloud },
+  components,
   template: '<StyleWrapper><SoundCloud track="370226924" /></StyleWrapper>'
 });
 
 export const playlist = () => ({
-  components: { SoundCloud },
+  components,
   template: '<StyleWrapper><SoundCloud playlist="73448639" /></StyleWrapper>'
 });
 
 export const mini = () => ({
-  components: { SoundCloud },
+  components,
   template: '<StyleWrapper><SoundCloud track="194881641" :mini="true" /></StyleWrapper>'
 });
 
 export const lightTheme = () => ({
-  components: { SoundCloud },
+  components,
   template: '<StyleWrapper><SoundCloud playlist="73448639" theme="light" /></StyleWrapper>'
 });
 
 export const multiplePlayers = () => ({
-  components: { SoundCloud },
+  components,
   template: '<StyleWrapper><SoundCloud track="370226924" /><SoundCloud track="194881641" /></StyleWrapper>'
+});
+
+export const events = () => ({
+  components: {
+    StyleWrapper,
+    Events
+  },
+  template: '<StyleWrapper><Events /></StyleWrapper>'
 });
